4172a616d4ca3c7043ea60987102f1e645140e22
oboril <jan.oboril@gmail.com>
2024-04-08 16:33:02 -0700

    trying to fix workflows

--- src/mocca2/__main__.py
+++ src/mocca2/__main__.py
@@ -15,13 +15,13 @@ def extract_bz2(filename, path):
 
 
 def download_data():
-    files = ["examples", "benzaldhyde"]
+    files = ["examples", "benzaldehyde", "diterpene_esters", "knoevenagel", "cyanation"]
 
     directory = impresources.files(example_data).joinpath("data")
     directory.mkdir(parents=True, exist_ok=True)
 
     for file in files:
-        url = f"https://raw.githubusercontent.com/oboril/mocca/blob/example-data/src/mocca2/example_data/data/{file}.tar.bzip2"
+        url = f"https://github.com/oboril/mocca/raw/example-data/src/mocca2/example_data/data/{file}.tar.bzip2"
         filename = directory.joinpath(f"{file}.tar.bz2")
 
         print(f"Downloading {file} data to {filename}")
