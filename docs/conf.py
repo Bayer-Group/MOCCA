c3c5bfebb7773ec5c61849920bb361691868c88c
oboril <jan.oboril@gmail.com>
2024-04-09 12:08:56 -0700

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,35 +6,36 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MOCCA2'
-copyright = '2023, Jan Oboril'
-author = 'Jan Oboril'
-release = '0.0.1'
+project = "MOCCA2"
+copyright = "2024, Jan Oboril"
+author = "Jan Oboril"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
 
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
+sys.path.insert(0, os.path.abspath("../src"))
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
 
-autodoc_member_order = 'bysource'
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+autodoc_member_order = "bysource"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 html_theme_options = {
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 2,
-    'includehidden': True,
-    'titles_only': False
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 2,
+    "includehidden": True,
+    "titles_only": False,
 }
