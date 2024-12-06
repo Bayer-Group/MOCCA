1b796f7d34c41acb318bae9e8081f42ffa6395fb
jpfolch <jpfolch@outlook.com>
2024-12-06 10:45:45 +0000

    added chemstation encoding options

--- src/mocca2/parsers/chemstation.py
+++ src/mocca2/parsers/chemstation.py
@@ -3,15 +3,15 @@ import numpy as np
 
 from mocca2.classes import Data2D
 
-def parse_chemstation(path) -> Data2D:
+def parse_chemstation(path, encoding='utf-16') -> Data2D:
     """
     Chemstation read and processing function.
     """
 
     if os.path.isfile(path):
-        raw = np.genfromtxt(path, delimiter=',', encoding='utf-16')
+        raw = np.genfromtxt(path, delimiter=',', encoding=encoding)
     else:
-        raw = np.genfromtxt(os.path.join(path, 'DAD1.CSV'), delimiter=',', encoding='utf-16')
+        raw = np.genfromtxt(os.path.join(path, 'DAD1.CSV'), delimiter=',', encoding=encoding)
 
 
     time = raw[1:,0]
